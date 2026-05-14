a981afc5484aa7fddbde2da1860bcd8999028987
ahmedgad 112 <gadoo007551@gmail.com>
2026-05-15 01:08:40 +0300

    2.3.2 V

--- database/migrations/2026_05_14_215338_add_event_column_to_activity_log_table.php
+++ database/migrations/2026_05_14_215338_add_event_column_to_activity_log_table.php
@@ -8,14 +8,15 @@ class AddEventColumnToActivityLogTable extends Migration
 {
     public function up()
     {
-        Schema::connection(config('activitylog.database_connection'))->table(config('activitylog.table_name'), function (Blueprint $table) {
+        // استبدلنا config باسم الجدول مباشرة
+        Schema::table('activity_log', function (Blueprint $table) {
             $table->string('event')->nullable()->after('subject_type');
         });
     }
 
     public function down()
     {
-        Schema::connection(config('activitylog.database_connection'))->table(config('activitylog.table_name'), function (Blueprint $table) {
+        Schema::table('activity_log', function (Blueprint $table) {
             $table->dropColumn('event');
         });
     }
